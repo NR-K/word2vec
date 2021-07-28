f849b4e497a5ee18bea886e8e0ff7d3a2c756a1f
NR-K <klvheve@gmail.com>
2021-07-28 13:46:29 +0900

    cnn model 추가

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -1,3 +1,5 @@
 import tensorflow_datasets as tfds
 import tnesorflow as tf
-pirnt(tf.__version__)
+
+dataset, info = tfds.load("imdb_reviews/subwords8k", with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
